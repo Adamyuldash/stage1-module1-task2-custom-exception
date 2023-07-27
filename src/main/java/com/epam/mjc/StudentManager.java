1356d9a13b100ed1ab4038c37c5c3d701242cfea
Adam Yuldashev <adamyuldashev1997@gmail.com>
2023-07-27 09:11:14 -0700

    o`zgartirdim mini2

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -3,21 +3,25 @@ package com.epam.mjc;
 
 public class StudentManager {
 
-  private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    getClass().getName();
+    public Student find(long studentID) throws CustomException {
 
-    return Student.getValueOf(studentID);
-  }
-
-  public static void main(String[] args) {
-    StudentManager manager = new StudentManager();
-
-    for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
+        for (int i = 0; i < IDs.length; i++) {
+            if (IDs[i] == studentID) {
+                return Student.getValueOf(studentID);
+            }
+        }
+        throw new CustomException("Could not find student with ID {id}" + studentID );
     }
 
-  }
+    public static void main(String[] args) {
+        StudentManager manager = new StudentManager();
+
+        for (int i = 0; i < IDs.length; i++) {
+            Student student = manager.find(IDs[i]);
+            System.out.println("Student name " + student.getName());
+        }
+
+    }
 }
